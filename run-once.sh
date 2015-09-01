ef00555114394b66357ac260a6f87744ab96d14b
epierre <epierre@e-nef.com>
2015-09-01 21:52:18 +0200

    script to be runned once on install

---
+++ run-once.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+sudo apt-get update
+sudo apt-get install libdancer-perl libfile-slurp-perl  liblwp-protocol-psgi-perl 
+sudo apt-get install libtime-piece-perl libjson-perl libplack-perl starman  libcrypt-ssleay-perl
+sudo apt-get install libdbd-sqlite3-perl libdbi-perl sqlite3
+sudo cp MyDomoAtHome.sh /etc/init.d/MyDomoAtHome.sh
+sudo chmod +x /etc/init.d/MyDomoAtHome.sh
+sudo update-rc.d MyDomoAtHome.sh defaults
+
+
