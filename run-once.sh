dacd1b08441c6add3b896264e535c692566da34c
epierre <epierre@e-nef.com>
2015-09-05 11:51:16 +0200

    new dependencies

--- run-once.sh
+++ run-once.sh
@@ -3,7 +3,7 @@ sudo apt-get update
 sudo apt-get install libdancer-perl libfile-slurp-perl  liblwp-protocol-psgi-perl 
 sudo apt-get install libtime-piece-perl libjson-perl libplack-perl starman  libcrypt-ssleay-perl libdatetime-perl
 sudo apt-get install libdbd-sqlite3-perl libdbi-perl sqlite3
-sudo apt-get install libaudio-mpd-perl libnet-upnp-perl
+sudo apt-get install libaudio-mpd-perl libnet-upnp-perl libpoe-component-client-mpd-perl 
 sudo cp MyDomoAtHome.sh /etc/init.d/MyDomoAtHome.sh
 sudo chmod +x /etc/init.d/MyDomoAtHome.sh
 sudo update-rc.d MyDomoAtHome.sh defaults
--- update-mdah.sh
+++ update-mdah.sh
@@ -3,5 +3,5 @@
 sudo service MyDomoAtHome.sh stop
 sleep 2
 git pull
-sudo apt-get install libaudio-mpd-perl libnet-upnp-perl
+sudo apt-get install libaudio-mpd-perl libnet-upnp-perl libpoe-component-client-mpd-perl 
 sudo service MyDomoAtHome.sh start
