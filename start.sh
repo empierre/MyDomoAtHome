99ef151ece3d5f7f530ba49205158afd94d87847
Emmanuel <epierre@e-nef.com>
2015-09-07 10:14:31 +0200

    Update start.sh
    moving to 3001 port

--- start.sh
+++ start.sh
@@ -8,4 +8,5 @@
 # Short-Description: Start imperihome  
 # Description:       Imperihome.  
 ### END INIT INFO
-nohup plackup -E production -s Starman --workers=2 -p 5001 -a bin/app.pl --pid /tmp/mydomoathome.pid &
+echo "please use the automatic start script to be installed with run_once.sh"
+nohup plackup -E production -s Starman --workers=2 -p 3001 -a bin/app.pl --pid /tmp/mydomoathome.pid &
