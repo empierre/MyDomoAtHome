55250bc079c465bfe9f43ffe7ad546af40b0bfc0
ODROID <epierre@e-nef.com>
2015-09-16 14:22:00 +0200

    adding whiptail

--- run-once.sh
+++ run-once.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
+clear
+
+
 sudo apt-get update
+
+sudo apt-get install whiptail
 sudo apt-get install libdancer-perl libfile-slurp-perl  liblwp-protocol-psgi-perl 
 sudo apt-get install libtime-piece-perl libjson-perl libplack-perl starman  libcrypt-ssleay-perl libdatetime-perl
 sudo apt-get install libdbd-sqlite3-perl libdbi-perl sqlite3
--- update-mdah.sh
+++ update-mdah.sh
@@ -1,7 +1,14 @@
 #!/bin/sh
+clear
+
+if [ $EUID -ne 0 ]; then
+        echo "You must run this script as root"
+        echo "Example: sudo $0"
+        exit
+fi
 
 sudo service MyDomoAtHome.sh stop
 sleep 2
 git pull
-sudo apt-get install libaudio-mpd-perl libnet-upnp-perl libpoe-component-client-mpd-perl 
+sudo apt-get install libaudio-mpd-perl libnet-upnp-perl libpoe-component-client-mpd-perl  whiptail
 sudo service MyDomoAtHome.sh start
