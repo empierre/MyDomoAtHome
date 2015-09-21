c0e5a52ad79c047e15eadc5e08801c94ee47ac49
Emmanuel PIERRE <epierre@e-nef.com>
2015-09-21 17:04:57 +0200

    beautifulling

--- run-once.sh
+++ run-once.sh
@@ -1,16 +1,25 @@
 #!/bin/sh
+echo "Will update packages list first..."
 clear
+echo "Will update packages list first..."
+#sudo apt-get update 2>&1 > /dev/null
+echo "Now getting the needed software..."
+sudo apt-get -y install whiptail  2>&1 > /dev/null
+
+ if ! whiptail  --backtitle "MyDomoAtHome ISS Interface" --yesno "Have you configured MyDomoAtHome.sh with your parameters before continuing ? Otherwise please say NO" 0 0; then
+                whiptail  --backtitle "MyDomoAtHome ISS Interface" --msgbox "Understood! Aborting!" 0 0 
+                return
+        fi
 
 
-sudo apt-get update
-
-sudo apt-get install whiptail
-sudo apt-get install libdancer-perl libfile-slurp-perl  liblwp-protocol-psgi-perl 
-sudo apt-get install libtime-piece-perl libjson-perl libplack-perl starman  libcrypt-ssleay-perl libdatetime-perl
-sudo apt-get install libdbd-sqlite3-perl libdbi-perl sqlite3
-sudo apt-get install libaudio-mpd-perl libnet-upnp-perl libpoe-component-client-mpd-perl 
+echo "Now getting getting and installing dependencies..."
+sudo apt-get install libdancer-perl libfile-slurp-perl  liblwp-protocol-psgi-perl  2>&1 
+sudo apt-get install libtime-piece-perl libjson-perl libplack-perl starman  libcrypt-ssleay-perl libdatetime-perl 2>&1 
+sudo apt-get install libdbd-sqlite3-perl libdbi-perl sqlite3 2>&1 
+sudo apt-get install libaudio-mpd-perl libnet-upnp-perl libpoe-component-client-mpd-perl  2>&1 
+echo "Now installing the service"
 sudo cp MyDomoAtHome.sh /etc/init.d/MyDomoAtHome.sh
 sudo chmod +x /etc/init.d/MyDomoAtHome.sh
 sudo update-rc.d MyDomoAtHome.sh defaults
-
+whiptail  --backtitle "MyDomoAtHome ISS Interface" --msgbox "Everything has been installed ! Please remember default port is 3001." 0 0 
 
