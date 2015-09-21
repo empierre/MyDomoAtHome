23f8de5a793e0026ac7c7517d027bdc7493d5e65
Emmanuel PIERRE <epierre@e-nef.com>
2015-09-21 17:10:49 +0200

    added beautifulling

--- update-mdah.sh
+++ update-mdah.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 clear
-
-
+echo "Stoping MyDomoAtHome service..."
 sudo service MyDomoAtHome.sh stop
-sleep 2
+echo "Retrieving latest code..."
 git pull
-sudo apt-get install libaudio-mpd-perl libnet-upnp-perl libpoe-component-client-mpd-perl  whiptail
+echo "Checking latest libraries dependencies..."
+sudo apt-get -y install libaudio-mpd-perl libnet-upnp-perl libpoe-component-client-mpd-perl  whiptail
+echo "Restarting MyDomoAtHome service..."
 sudo service MyDomoAtHome.sh start
+echo "Update path finished"
