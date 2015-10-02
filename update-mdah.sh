c40fb2d0a3739eff08c9da15a04de29d09e684e3
Emmanuel PIERRE <epierre@e-nef.com>
2015-10-02 23:22:14 +0200

    dependencies cpanminus

--- update-mdah.sh
+++ update-mdah.sh
@@ -5,8 +5,9 @@ sudo service MyDomoAtHome.sh stop
 echo "Retrieving latest code..."
 git pull
 echo "Checking latest libraries dependencies..."
-sudo apt-get -y install libaudio-mpd-perl libnet-upnp-perl libpoe-component-client-mpd-perl  whiptail
+sudo apt-get -y install libaudio-mpd-perl libnet-upnp-perl libpoe-component-client-mpd-perl  whiptail cpanminus
 sudo apt-get -y install libtime-piece-perl libjson-perl libplack-perl starman libcrypt-ssleay-perl libdatetime-perl libdancer2-perl libswitch-perl  2>&1 
+sudo cpanm Dancer2
 echo "Restarting MyDomoAtHome service..."
 sudo service MyDomoAtHome.sh start
 echo "Update path finished"
