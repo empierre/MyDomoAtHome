ea4a6cb79a0b6bfd8d2765b5a8ebc6caeed4a8f7
epierre <epierre@e-nef.com>
2015-10-06 22:43:38 +0200

    removed libdancer2 as not available

--- update-mdah.sh
+++ update-mdah.sh
@@ -6,7 +6,7 @@ echo "Retrieving latest code..."
 git pull
 echo "Checking latest libraries dependencies..."
 sudo apt-get -y install libaudio-mpd-perl libnet-upnp-perl libpoe-component-client-mpd-perl  whiptail cpanminus
-sudo apt-get -y install libtime-piece-perl libjson-perl libplack-perl starman libcrypt-ssleay-perl libdatetime-perl libdancer2-perl libswitch-perl  2>&1 
+sudo apt-get -y install libtime-piece-perl libjson-perl libplack-perl starman libcrypt-ssleay-perl libdatetime-perl libswitch-perl  2>&1 
 sudo cpanm Time::Moment
 curl -L http://cpanmin.us | perl - --sudo Dancer2
 echo "Restarting MyDomoAtHome service..."
