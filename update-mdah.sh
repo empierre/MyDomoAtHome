72e9fd466513e4ac0c718baf19326da030d10328
Emmanuel <epierre@e-nef.com>
2015-10-12 09:52:18 +0200

    Update update-mdah.sh
    typo error

--- update-mdah.sh
+++ update-mdah.sh
@@ -7,7 +7,7 @@ git pull
 echo "Checking latest libraries dependencies..."
 sudo apt-get -y install libaudio-mpd-perl libnet-upnp-perl libpoe-component-client-mpd-perl  whiptail cpanminus
 sudo apt-get -y install libtime-piece-perl libjson-perl libplack-perl starman libcrypt-ssleay-perl libdatetime-perl libswitch-perl  2>&1 
-./spedUP.sh
+./speedUP.sh
 sudo cpanm Time::Moment
 #curl -L http://cpanmin.us | perl - --sudo Dancer2
 echo "Restarting MyDomoAtHome service..."
