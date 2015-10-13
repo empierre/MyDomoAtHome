bef9e0cc78a1f4f969568f3360da75142de222f4
epierre <epierre@e-nef.com>
2015-10-13 22:15:01 +0200

    default pi path

--- MyDomoAtHome.sh
+++ MyDomoAtHome.sh
@@ -13,7 +13,7 @@
  
 PATH=/bin:/usr/bin:/sbin:/usr/sbin:$HOME/perl5/bin
 DAEMON="/usr/bin/plackup"
-APP_DIR="/home/in/test/testenv/"
+APP_DIR="/home/pi/domoticz/"
 NAME="MyDomoAtHome"
 PIDFILE=/var/run/$NAME.pid
 PORT=3001
