89a334c4b037de19627aa2d57c06dd3ee06067da
Emmanuel <epierre@e-nef.com>
2015-10-15 23:12:16 +0200

    Update MyDomoAtHome.sh

--- MyDomoAtHome.sh
+++ MyDomoAtHome.sh
@@ -13,7 +13,7 @@
  
 PATH=/bin:/usr/bin:/sbin:/usr/sbin:$HOME/perl5/bin
 DAEMON="/usr/bin/plackup"
-APP_DIR="/home/in/domoticz/MyDomoAtHome/"
+APP_DIR="/home/pi/domoticz/MyDomoAtHome/"
 NAME="MyDomoAtHome"
 PIDFILE=/var/run/$NAME.pid
 PORT=3001
