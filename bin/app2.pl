f126f47b851f21059592b7f17a4f5e0664672564
epierre <epierre@e-nef.com>
2015-11-04 21:59:59 +0100

    cleanup

--- bin/app2.pl
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env perl
-use strict;
-use warnings;
-use FindBin;
-use lib "$FindBin::Bin/../lib";
-
-use Domo2;
-Domo2->to_app;
--- fp/b.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-fatpack trace --use=Dancer2 ../bin/app2.pl
-fatpack packlists-for `cat fatpacker.trace` > packlists
-fatpack tree `cat packlists`
-(fatpack file; cat ../bin/app2.pl) > myapp2.pl  
-tar cvzf app2.tgz *
-cp app2.tgz /media/usb1
