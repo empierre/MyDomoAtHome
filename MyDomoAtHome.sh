9176e27907b306e6c167c52dfdb0a653b1b4ecf5
Emmanuel <epierre@e-nef.com>
2015-11-06 10:33:58 +0100

    Update MyDomoAtHome.sh

--- MyDomoAtHome.sh
+++ MyDomoAtHome.sh
@@ -9,126 +9,55 @@
 # Description:       Imperihome.  
 ### END INIT INFO
 
-# Start a Plack daemon.
- 
-# CHANGE TO YOUR PATH HERE
-APP_DIR="/home/pi/domoticz/"
-PORT=3001
-# DO NOT CHANGE BELOW
-PATH=/bin:/usr/bin:/sbin:/usr/sbin:$HOME/perl5/bin
-DAEMON="/usr/bin/plackup"
-NAME="MyDomoAtHome"
-PIDFILE=/var/run/$NAME.pid
+. /etc/rc.d/init.d/functions
 
-USER=www-data
-GROUP=www-data
+USER="www-data"
 
-CWD=`pwd`
-# Defaults
-RUN="no"
-OPTIONS="-E production -s Starman --workers=4 --user $USER --group $GROUP --port $PORT -a $APP_DIR/MyDomoAtHome/bin/app.pl" 
- 
-#
-# These compatibility funcs are here just for sarge backports.
-# They will be removed post-etch.
-#
-log_daemon_msg() {
-echo -n "$1: $2"
-}
- 
-log_end_msg() {
-if [ $1 -ne 0 ]; then
-echo " failed!"
-else
-echo "."
-fi
-}
- 
-[ -f /lib/lsb/init-functions ] && . /lib/lsb/init-functions
- 
-start()
+DAEMON="/usr/bin/node"
+ROOT_DIR="/home/pi/domoticz/MyDomoAtHome"
+
+SERVER="$ROOT_DIR/mdah.js"
+LOG_FILE="/var/log/mdah.log"
+
+LOCK_FILE="/var/lock/subsys/mdah"
+
+do_start()
 {
-log_daemon_msg "Starting plack server" "$NAME"
-start-stop-daemon -b -m --start --quiet --pidfile "$PIDFILE" --exec $DAEMON -- $OPTIONS
-if [ $? != 0 ]; then
-log_end_msg 1
-exit 1
-else
-log_end_msg 0
-fi
+        if [ ! -f "$LOCK_FILE" ] ; then
+                echo -n $"Starting $SERVER: "
+                runuser -l "$USER" -c "$DAEMON $SERVER >> $LOG_FILE &" && echo_success || echo_failure
+                RETVAL=$?
+                echo
+                [ $RETVAL -eq 0 ] && touch $LOCK_FILE
+        else
+                echo "$SERVER is locked."
+                RETVAL=1
+        fi
 }
- 
-signal()
+do_stop()
 {
- 
-if [ "$1" = "stop" ]; then
-SIGNAL="TERM"
-log_daemon_msg "Stopping plack server" "$NAME"
-else
-if [ "$1" = "reload" ]; then
-SIGNAL="HUP"
-log_daemon_msg "Reloading plack server" "$NAME"
-else
-echo "ERR: wrong parameter given to signal()"
-exit 1
-fi
-fi
-if [ -f "$PIDFILE" ]; then
-start-stop-daemon --stop --signal $SIGNAL --quiet --pidfile "$PIDFILE"
-if [ $? = 0 ]; then
-log_end_msg 0
-else
-SIGNAL="KILL"
-start-stop-daemon --stop --signal $SIGNAL --quiet --pidfile "$PIDFILE"
-if [ $? != 0 ]; then
-log_end_msg 1
-[ $2 != 0 ] || exit 0
-else
-rm "$PIDFILE"
-log_end_msg 0
-fi
-fi
-if [ "$SIGNAL" = "KILL" ] || [ "$SIGNAL" = "TERM" ]; then
-rm -f "$PIDFILE"
-fi
-else
-log_end_msg 0
-fi
+        echo -n $"Stopping $SERVER: "
+        pid=`ps -aefw | grep "$DAEMON $SERVER" | grep -v " grep " | awk '{print $2}'`
+        kill -9 $pid > /dev/null 2>&1 && echo_success || echo_failure
+        RETVAL=$?
+        echo
+        [ $RETVAL -eq 0 ] && rm -f $LOCK_FILE
 }
- 
+
 case "$1" in
-start)
-start
-;;
- 
-force-start)
-start
-;;
- 
-stop)
-signal stop 0
-;;
- 
-force-stop)
-signal stop 0
-;;
- 
-reload)
-signal reload 0
-;;
- 
-force-reload|restart)
-signal stop 1
-sleep 2
-start
-;;
-status)
-status_of_proc -p $PIDFILE $DAEMON "$NAME process" && exit 0 || exit $?
-;;
-*)
-echo "Usage: /etc/init.d/$NAME {start|force-start|stop|status|force-stop|reload|restart|force-reload}"
-exit 1
-;;
+        start)
+                do_start
+                ;;
+        stop)
+                do_stop
+                ;;
+        restart)
+                do_stop
+                do_start
+                ;;
+        *)
+                echo "Usage: $0 {start|stop|restart}"
+                RETVAL=1
 esac
- 
-exit 0
+
+exit $RETVAL
