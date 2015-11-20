13105d4b5065cb240e2a0d8ac5849971320fc375
epierre <epierre@e-nef.com>
2015-11-20 18:16:06 +0100

    updated

--- start2.sh
+++ start2.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 # if your application is not installed in @INC path:
-export PERL5LIB='~/domoticz/MyDomoAtHome/lib/'
+export PERL5LIB='/home/pi/domoticz/MyDomoAtHome/lib/'
 exec 2>&1 \
- sudo -u www-data plackup -E development -s Starman --workers=2 -p 3001  -a bin/app.pl
+ sudo -u www-data plackup -E development -s Starman --workers=2 -p 3001  -a /home/pi/domoticz/MyDomoAtHome/bin/app.pl
