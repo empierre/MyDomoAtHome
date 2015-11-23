a0128c10ef2388453f69c8b842eec4a3dc9f786a
Emmanuel <epierre@e-nef.com>
2015-11-23 13:09:06 +0100

    Update run-once.sh
    workaround for 3350 domoticz image issue

--- run-once.sh
+++ run-once.sh
@@ -18,7 +18,8 @@
 ################################################################################
 clear
 echo "Will update packages list first..."
-#sudo apt-get update 2>&1 > /dev/null
+sudo dpkg --clear-avail
+sudo apt-get update 2>&1 > /dev/null
 echo "Now getting the needed software..."
 sudo apt-get -y install whiptail  2>&1 > /dev/null
 
@@ -29,7 +30,9 @@ sudo apt-get -y install whiptail  2>&1 > /dev/null
 
 
 echo "Now getting getting and installing dependencies..."
-sudo apt-get -y install libdancer-perl libswitch-perl libfile-slurp-perl  liblwp-protocol-psgi-perl cpanminus 2>&1 
+sudo apt-get -y install cpanminus 2>&1 
+sudo cpanm Task::Plack
+sudo apt-get -y install libdancer-perl libswitch-perl libfile-slurp-perl  liblwp-protocol-psgi-perl  2>&1 
 sudo apt-get -y install libtime-piece-perl libjson-perl libplack-perl starman  libcrypt-ssleay-perl libdatetime-perl 2>&1 
 sudo apt-get -y install libdbd-sqlite3-perl libdbi-perl sqlite3 2>&1 
 sudo apt-get -y install libaudio-mpd-perl libnet-upnp-perl libpoe-component-client-mpd-perl  2>&1 
