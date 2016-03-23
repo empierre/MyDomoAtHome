b8a7b7cdb76ca228a4bdc4c3c68959901d8b4e2d
epierre <epierre@e-nef.com>
2016-03-23 23:04:05 +0100

    code fix to run on nodejs <1

--- mdah.js
+++ mdah.js
@@ -40,7 +40,7 @@ var app = express();
 //working variaboles
 var last_version_dt;
 var last_version =getLastVersion();
-var ver="0.0.31";
+var ver="0.0.32";
 var device_tab={};
 var room_tab=[];
 var device = {MaxDimLevel : null,Action:null,graph:null,Selector:null};
@@ -79,7 +79,52 @@ if (! nconf.get('domo_path')) {
     app.set('port', process.env.PORT || nconf.get('port'));
 }
 
+function versionCompare(v1, v2, options) {
+    var lexicographical = options && options.lexicographical,
+        zeroExtend = options && options.zeroExtend,
+        v1parts = v1.split('.'),
+        v2parts = v2.split('.');
 
+    function isValidPart(x) {
+        return (lexicographical ? /^\d+[A-Za-z]*$/ : /^\d+$/).test(x);
+    }
+
+    if (!v1parts.every(isValidPart) || !v2parts.every(isValidPart)) {
+        return NaN;
+    }
+
+    if (zeroExtend) {
+        while (v1parts.length < v2parts.length) v1parts.push("0");
+        while (v2parts.length < v1parts.length) v2parts.push("0");
+    }
+
+    if (!lexicographical) {
+        v1parts = v1parts.map(Number);
+        v2parts = v2parts.map(Number);
+    }
+
+    for (var i = 0; i < v1parts.length; ++i) {
+        if (v2parts.length == i) {
+            return 1;
+        }
+
+        if (v1parts[i] == v2parts[i]) {
+            continue;
+        }
+        else if (v1parts[i] > v2parts[i]) {
+            return 1;
+        }
+        else {
+            return -1;
+        }
+    }
+
+    if (v1parts.length != v2parts.length) {
+        return -1;
+    }
+
+    return 0;
+}
 function getLastVersion() {
     var now = moment();
     if ((last_version_dt)&&(last_version_dt.isBefore(moment().add(4,'h')))) {
@@ -1790,7 +1835,8 @@ for (var k in interfaces) {
         }
     }
 }
-if (process.versions.node<1) {
+
+if (versionCompare(process.versions.node,'1.0.0')<0) {
 	logger.info("Hostname: "+os.hostname()+" "+my_ip);
 } else {
 	logger.info("Hostname: "+os.hostname()+" "+my_ip+" in "+os.homedir());
