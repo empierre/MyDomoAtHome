ad04ad8b6a95991c773877bccca1c2754edac79f
epierre <epierre@e-nef.com>
2016-03-24 21:50:27 +0100

    updated

--- correct.sh
+++ correct.sh
@@ -9,5 +9,14 @@ cd ./packaging/mdah/ && sudo bash ./redeb.sh
 cd ../..
 mv -f ./packaging/mdah/node-mydomoathome-1.deb ./binary/
 cp -f ./binary/node-mydomoathome-1.deb ./binary/node-mydomoathome-latest.deb
-cp -f ./binary/node-mydomoathome-1.deb ./binary/node-mydomoathome-$1.deb
+mv -f ./binary/node-mydomoathome-1.deb ./binary/node-mydomoathome-$1.deb
+cd binary
+dpkg-sig -k A5435C9B --sign builder node-mydomoathome-$1.deb
+dpkg-sig -k A5435C9B --sign builder node-mydomoathome-latest.deb
+rm -f Packages Packages.gz Release InRelease Release.gpg
+apt-ftparchive packages . > Packages
+gzip -c Packages > Packages.gz
+apt-ftparchive release . >Release
+gpg --clearsign -o InRelease Release
+gpg -abs -o Release.gpg Release
 sitecopy -u mdah
