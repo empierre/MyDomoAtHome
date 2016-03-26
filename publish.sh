c527aa1337551efae3709cc6c59c8be9101f2eb3
epierre <epierre@e-nef.com>
2016-03-26 17:47:04 +0100

    change start install

--- publish.sh
+++ publish.sh
@@ -3,12 +3,12 @@ if [ $# -eq 0 ]
   then
     echo "No arguments supplied"
 fi
-git commit -a
-git tag -a "v$1" -m "Release of version $1"
+#git tag -a "v$1" -m "Release of version $1"
+npm version $1
 ./git-release.sh $1
+git commit -a
 git push origin --tags
 git push
-npm version $1
 ver=$1
 perl -pi -e "s/Version: .*/Version: ${ver}/" packaging/mdah/deb-src/DEBIAN/control
 perl -pi -e "s/^RUN wget.*/RUN wget http:\/\/www.e-nef.com\/domoticz\/mdah\/node-mydomoathome-${ver}.deb/" Dockerfile
@@ -30,5 +30,5 @@ gpg -abs -o Release.gpg Release
 sitecopy -u mdah
 cd ..
 npm publish 
-curl -X POST --data-urlencode 'payload={"channel": "#general", "username": "webhookbot", "text": "New package version '"$1"' available at <http://www.e-nef.com/domoticz/mdah/node-mydomoathome-latest.deb|node-mydomoathome-latest.deb>", "icon_emoji": ":ghost:"}' https://hooks.slack.com/services/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
+curl -X POST --data-urlencode 'payload={"channel": "#general", "username": "webhookbot", "text": "New package version '"$1"' available at <http://www.e-nef.com/domoticz/mdah/node-mydomoathome-latest.deb|node-mydomoathome-'"$1"'.deb>", "icon_emoji": ":ghost:"}' https://hooks.slack.com/services/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
 ./make.docker.sh
