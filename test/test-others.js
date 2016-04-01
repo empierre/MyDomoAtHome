8ba686c52b97d8b90e916cf608d16392357e71d8
epierre <epierre@e-nef.com>
2016-04-01 22:44:14 +0200

    some more tests

--- test/test-others.js
+++ test/test-others.js
@@ -1,17 +1,17 @@
 'use strict';
 
 var apiPort = process.env.API_PORT||3002;
-var apiURL = 'http://localhost:' + apiPort + '/system';
-var apiURL2 = 'http://localhost:' + apiPort + '/rooms';
-var apiURL3 = 'http://localhost:' + apiPort + '/devices';
+var apiURL = 'http://localhost:' + apiPort + '/devices/10/action/setStatus/0';
+var apiURL2 = 'http://localhost:' + apiPort + '/devices/447/action/setLevel/100';
+var apiURL3 = 'http://localhost:' + apiPort + '/devices/190/action/setSetPoint/1';
 
 var superagent = require('superagent');
 var chai = require('chai');
 var expect = chai.expect;
 var should = require('should');
 
-describe('System', function () {
-  it('Testing call to ' + apiURL, function (done) {
+describe('Setters', function () {
+  it('Testing Index call to ' + apiURL, function (done) {
     superagent.get(apiURL)
       .end(function (err, res) {
         (err === null).should.equal(true);
@@ -20,8 +20,8 @@ describe('System', function () {
       });
   });
 });
-describe('Rooms', function () {
-  it('Testing call to ' + apiURL2, function (done) {
+describe('Setters', function () {
+  it('Testing Index call to ' + apiURL2, function (done) {
     superagent.get(apiURL2)
       .end(function (err, res) {
         (err === null).should.equal(true);
@@ -30,8 +30,8 @@ describe('Rooms', function () {
       });
   });
 });
-describe('Devices', function () {
-  it('Testing call to ' + apiURL3, function (done) {
+describe('Setters', function () {
+  it('Testing Index call to ' + apiURL3, function (done) {
     superagent.get(apiURL3)
       .end(function (err, res) {
         (err === null).should.equal(true);
