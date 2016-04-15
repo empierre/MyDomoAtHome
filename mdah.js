6d015aa7be5b63c2a2068ec80b78781f58cda1f5
Emmanuel <epierre@e-nef.com>
2016-04-15 14:21:08 +0200

    Update mdah.js
    put back graph P1 smart meter

--- mdah.js
+++ mdah.js
@@ -1970,13 +1970,30 @@ app.get("/devices/:deviceId/:paramKey/histo/:startdate/:enddate", function (req,
                         params.push(feeds);
                     }
                 } else if (paramKey === 'ConsoTotal') {
-                    key = 'v';
-                    for (var i = 0; i < data.result.length; i++) {
-                        var value = data.result[i][key];
-                        var dt = moment(data.result[i].d, 'YYYY-MM-DD HH:mm:ss').valueOf();
-                        var feeds = {"date": dt, "value": value};
-                        params.push(feeds);
-                    }
+                   if (ptype==='p1 smart meter') {
+			key = 'c';
+			var key2 ='c3';
+			for (var i = 0; i < data.result.length; i++) {
+				if ((range === 'month') || (range === 'year')) {
+					var value = (parseFloat(data.result[i][key]) + parseFloat(data.result[i][key2])) / 2;
+					var dt = moment(data.result[i].d, 'YYYY-MM-DD HH:mm:ss').valueOf();
+					var feeds = {"date": dt, "value": value};
+					params.push(feeds);
+				} else {
+					var value = data.result[i][key];
+					var dt = moment(data.result[i].d, 'YYYY-MM-DD HH:mm:ss').valueOf();
+					var feeds = {"date": dt, "value": value};
+					params.push(feeds);
+				}
+			}
+ 		} else {
+			key = 'v';
+			for (var i = 0; i < data.result.length; i++) {
+				var value = data.result[i][key];
+				var dt = moment(data.result[i].d, 'YYYY-MM-DD HH:mm:ss').valueOf();
+				var feeds = {"date": dt, "value": value};
+				params.push(feeds);
+			}
                 } else if (paramKey === 'Watts') {
                         key = 'v'+PLine;
                         var key2 ='v'+(parseInt(PLine)+3);
