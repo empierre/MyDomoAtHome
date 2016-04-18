c7ca1c5752daf2fbc66660a159822b321c781ff6
epierre <epierre@e-nef.com>
2016-04-18 20:08:06 +0200

    updated

--- packaging/make.spk.sh
+++ packaging/make.spk.sh
@@ -5,16 +5,16 @@ if [ $# -eq 0 ]
 fi
 ver=$1
 cd synology/
-cp ../../MyDomoAtHome/mdah.js app/
-cp ../../MyDomoAtHome/package.json app/
-cp ../../MyDomoAtHome/README.md app/
-rsync -a ../../MyDomoAtHome/public/ public/ --delete
-rsync -a ../../MyDomoAtHome/routes/ routes/ --delete
-tar cvzf package.tgz .
-perl -pi -e "s/version=.*/version="${ver}"/" INFO
-md5=($(md5sum packages.tgz))
-perl -pi -e "s/checksum=.*/checksum="${md5}"/" INFO
+cp ../../../MyDomoAtHome/mdah.js app/
+cp ../../../MyDomoAtHome/package.json app/
+cp ../../../MyDomoAtHome/README.md app/
+rsync -a ../../../MyDomoAtHome/public/ public/ --delete
+rsync -a ../../../MyDomoAtHome/routes/ routes/ --delete
+cd .. &&tar cvzf package.tgz synology && cd synology && mv -f ../package.tgz .
+perl -pi -e "s/version=.*/version=\"${ver}\"/" INFO
+md5=($(md5sum package.tgz))
+perl -pi -e "s/checksum=.*/checksum=\"${md5}\"/" INFO
 cd ..
 tar cvf node-mydomoathome-$ver.spk synology/
-dpkg-sig -k A5435C9B --sign builder node-mydomoathome-$ver.spk
-mv -f ./packaging/node-mydomoathome-$ver.spk ../binary/
+#dpkg-sig -k A5435C9B --sign builder node-mydomoathome-$ver.spk
+mv -f ./node-mydomoathome-$ver.spk ../binary/
