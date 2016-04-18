7c18aa4944d80f28e704c201dd81ac39b6d9f786
Emmanuel <epierre@e-nef.com>
2016-04-18 15:22:18 +0200

    Update make.spk.sh

--- packaging/make.spk.sh
+++ packaging/make.spk.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
+if [ $# -eq 0 ]
+  then
+    echo "No arguments supplied"
+fi
+ver=$1
 cd packaging/synology/
 tar cvzf package.tgz .
 perl -pi -e "s/version=.*/version="${ver}"/" INFO
-md5sum packages.tgz | perl -pi -e "s/checksum=.*/checksum="-"/" INFO
+md5=($(md5sum packages.tgz))
+perl -pi -e "s/checksum=.*/checksum="${md5}"/" INFO
 cd ..
-tar cvf node-mydomoathome-$1.spk synology/
-dpkg-sig -k A5435C9B --sign builder node-mydomoathome-$1.spk
-mv -f ./packaging/node-mydomoathome-$1.spk ../binary/
+tar cvf node-mydomoathome-$ver.spk synology/
+dpkg-sig -k A5435C9B --sign builder node-mydomoathome-$ver.spk
+mv -f ./packaging/node-mydomoathome-$ver.spk ../binary/
