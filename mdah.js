08e8ce3c445ba76338c94d13cc6b0f36441a56d9
epierre <epierre@e-nef.com>
2020-02-20 22:24:26 +0100

    upd

--- mdah.js
+++ mdah.js
@@ -1,6 +1,6 @@
 //##############################################################################
 //  This file is part of MyDomoAtHome - https://github.com/empierre/MyDomoAtHome
-//      Copyright (C) 2014-2018 Emmanuel PIERRE (domoticz@e-nef.com)
+//      Copyright (C) 2014-2020 Emmanuel PIERRE (domoticz@e-nef.com)
 //
 // MyDomoAtHome is free software: you can redistribute it and/or modify
 //  it under the terms of the GNU General Public License as published by
